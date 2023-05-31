135f64edbab30bee27e5e11c959cd76f7653167d
Aleks <99113475+A-M-Mitev@users.noreply.github.com>
2023-05-31 23:00:17 +0300

    Added teleportation

--- AlphaMatrix.py
+++ AlphaMatrix.py
@@ -6,10 +6,14 @@ from pygame.locals import (
     K_DOWN,
     K_LEFT,
     K_RIGHT,
-    K_e,
     K_ESCAPE,
     KEYDOWN,
     QUIT,
+    K_w,
+    K_a,
+    K_d,
+    K_s,
+    K_LSHIFT,
 )
 
 # Constants
@@ -34,50 +38,66 @@ class Player(pygame.sprite.Sprite):
         hitbox_height = self.rect.height
         # Create the hitbox surface
         self.hitbox = pygame.Surface((hitbox_width, hitbox_height))
-        self.hitbox.fill((0, 0, 0))
+        self.hitbox.fill((255, 0, 0))
         self.rect_hitbox = self.hitbox.get_rect()
         self.rect_hitbox.center = self.rect.center
+        # Cooldown for teleportation ability
+        self.teleport_cooldown = 750
+        self.last_teleport_time = pygame.time.get_ticks()
 
-    # Movement
+    # Basic movement
     def update(self, pressed_keys):
-        if pressed_keys[K_UP]:
-            self.rect.move_ip(0, -10)
-            self.rect_hitbox.move_ip(0, -10)
-        if pressed_keys[K_DOWN]:
-            self.rect.move_ip(0, 10)
-            self.rect_hitbox.move_ip(0, 10)
-        # Makes sure the hitbox stays in the center of the image
-        if pressed_keys[K_LEFT] and self.rect.left > 5:
-            self.rect.move_ip(-10, 0)
-            self.rect_hitbox.move_ip(-10, 0)
-        if pressed_keys[K_RIGHT] and self.rect.right < SCREEN_WIDTH - 5:
-            self.rect.move_ip(10, 0)
-            self.rect_hitbox.move_ip(10, 0)
+        if pressed_keys[K_w]:
+            self.rect.move_ip(0, -15)
+            self.rect_hitbox.move_ip(0, -15)
+        if pressed_keys[K_s]:
+            self.rect.move_ip(0, 15)
+            self.rect_hitbox.move_ip(0, 15)
+        if pressed_keys[K_a]:
+            self.rect.move_ip(-15, 0)
+            self.rect_hitbox.move_ip(-15, 0)
+        if pressed_keys[K_d]:
+            self.rect.move_ip(15, 0)
+            self.rect_hitbox.move_ip(15, 0)
 
-        # Window borders
+        # Teleportation ability 
+        teleport_distance = 500  # Distance to teleport
+        current_time = pygame.time.get_ticks()
+        time_since_last_teleport = current_time - self.last_teleport_time
+
+        if time_since_last_teleport >= self.teleport_cooldown:
+            if pressed_keys[K_UP]:
+                self.rect.y -= teleport_distance 
+                self.rect_hitbox.y -= teleport_distance
+            if pressed_keys[K_DOWN]:
+                self.rect.y += teleport_distance 
+                self.rect_hitbox.y += teleport_distance
+            if pressed_keys[K_LEFT]:
+                self.rect.x -= teleport_distance 
+                self.rect_hitbox.x -= teleport_distance
+            if pressed_keys[K_RIGHT]:
+                self.rect.x += teleport_distance  
+                self.rect_hitbox.x += teleport_distance
+            self.last_teleport_time = current_time
+
+
+        # Window borders for image
         if self.rect.left < 0:
             self.rect.left = 0
-          #  self.rect_hitbox.left = 0
         if self.rect.right > SCREEN_WIDTH:
             self.rect.right = SCREEN_WIDTH
-           # self.rect_hitbox.right = SCREEN_WIDTH
         if self.rect.top <= 0:
             self.rect.top = 0
             self.rect_hitbox.top = 0
         if self.rect.bottom >= SCREEN_HEIGHT:
             self.rect.bottom = SCREEN_HEIGHT
             self.rect_hitbox.bottom = SCREEN_HEIGHT
+        # Window borders for hitbox
+        if self.rect_hitbox.left < 26:
+            self.rect_hitbox.left = 26
+        if self.rect_hitbox.right > SCREEN_WIDTH - 26:
+            self.rect_hitbox.right = SCREEN_WIDTH - 26
     
-        # Window borders
-        if self.rect.left < 0:
-            self.rect.left = 0
-        if self.rect.right > SCREEN_WIDTH:
-            self.rect.right = SCREEN_WIDTH
-        if self.rect.top <= 0:
-            self.rect.top = 0
-        if self.rect.bottom >= SCREEN_HEIGHT:
-            self.rect.bottom = SCREEN_HEIGHT
-
 # Places a random symbol on given cordinates
 class Symbol(pygame.sprite.Sprite):
     def __init__(self, x_cord, y_cord, age):
@@ -114,6 +134,7 @@ class Chain(pygame.sprite.Sprite):
 
     def __init__(self):
         super(Chain, self).__init__()
+        # Lenght of chain is between 7 and 15 symbols
         self.age_of_chain = random.randint(7, 15)
         self.age = self.age_of_chain
         self.x_cord = random.randint(0, SCREEN_WIDTH)
@@ -204,7 +225,7 @@ while running:
     for entity in all_sprites:
         screen.blit(entity.surf, entity.rect)
     screen.blit(player.image, player.rect)
-    # screen.blit(player.hitbox, player.rect_hitbox) 
+    #screen.blit(player.hitbox, player.rect_hitbox) # <-- Shows hitbox
     
     # Checks if any of the symbols collide with the player's hitbox
     # by using their rectangles
@@ -213,6 +234,6 @@ while running:
             running = False
 
     pygame.display.flip()
-    clock.tick(30)
+    clock.tick(60)
 
 pygame.quit()
